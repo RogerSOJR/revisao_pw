5ca306886bc0768e980ccf3a8421748946e37526
RogerSOJR <rogerio.oliveira144@etec.sp.gov.br>
2022-02-21 22:56:30 -0300

    first commit

---
+++ js/carro.js
@@ -0,0 +1,14 @@
+function vendaCarro() {
+	alert("Desculpe, não temos carros disponíveis para compra!");
+}
+
+function troca(id1, id2) {
+	document.getElementById(id1).classList.add("d-none");
+	document.getElementById(id2).classList.remove("d-none");
+}
+
+function surpresa() {
+	const url =
+		"https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjW9viSkpL2AhVIH7kGHbJVDLUQFnoECA0QAQ&url=https%3A%2F%2Fbit.ly%2F2Z6cJDO&usg=AOvVaw2n0mfdxA60Ap9c_90s80DR";
+	window.open(url);
+}
